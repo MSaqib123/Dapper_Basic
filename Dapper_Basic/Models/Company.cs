b72ba173e900a915528a919faf20848ed6d75f5c
MBoota <m43577535@gmail.com>
2023-11-11 20:09:15 +0500

    Add project files.

---
+++ Dapper_Basic/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dapper_Basic.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Address { get; set; }
+        public int City { get; set; }
+        public int State { get; set; }
+        public int PostalCode { get; set; }
+    }
+}
